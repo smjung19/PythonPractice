cfd758dbd3294d7519bac262bd736a2273955f5c
smjung19 <118870712+smjung19@users.noreply.github.com>
2023-02-28 18:59:44 +0900

    GTRefmover.py
    Google Translate & Reference Remover
    1. Removes reference markers(e.g. [1], (2-3), 4-5).
    2. Uses Google Translate API to translate trimmed English papers into Korean.
    3. Automatically performs copy & paste into clipboard.

--- GTRefmover.py
+++ GTRefmover.py
@@ -10,11 +10,11 @@ from google.cloud import translate_v2 as translate
 
 # ====================================================================================================
 
-my_path = os.path.dirname(os.path.realpath(__file__))                                                   # .py 파일 위치
+my_path = os.path.dirname(os.path.realpath(__file__))                                                       # .py 파일 위치
 
-CREDENTIALS_PATH = my_path + "\\credentials.json"                                                       # 개별 Credential 이용-교체 필요
+CREDENTIALS_PATH = my_path + "\\credentials.json"                                                           # 개별 Credential 이용-교체 필요
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS_PATH                                         # 환경변수에 Credential 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS_PATH                                             # 환경변수에 Credential 등록
 
 # ====================================================================================================
 
@@ -25,29 +25,28 @@ def translate_text(target, text):
         text = text.decode("UTF-8")
 
     result = translate_client.translate(text, target_language=target)
-    return_value = "         \n" + result["translatedText"]
-    return return_value
+    return result["translatedText"]
     #print(u"Text: {}".format(result["input"]))
     #print(u"Translation: {}".format(result["translatedText"]))
     #print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
 
-def PyRefmover():                                                                                       # 각주 표시 제거 함수
-    tmp_str = clipboard.paste()                                                                         # Clipboard에서 복사
-    tmp_str = tmp_str.replace('\r', '').replace('\n', ' ')                                              # 줄바꿈 제거
+def PyRefmover():                                                                                           # 각주 표시 제거 함수
+    tmp_str = clipboard.paste()                                                                             # Clipboard에서 복사
+    tmp_str = tmp_str.replace('\r', '').replace('\n', ' ')                                                  # 줄바꿈 제거
     tmp_str = tmp_str.replace('Į','α').replace('ȕ','β').replace('Ȗ', 'γ')
     for i in my_dict:
         tmp_str = tmp_str.replace(i, i + '@')
     tmp_str = re.sub(r'\ (\d{1,2})\ ', ' \\1@ ', tmp_str)
-    tmp_str = re.sub(r'([a-z].*?)(\d[ 0-9,–\-]*\d+)([,.\ ])', '\\1\\3', tmp_str)                         # 각주 제거
-    tmp_str = re.sub(r'\ [ 0-9,–\-]*([ ,.])', '\\1', tmp_str)                                                #  각주 제거
-    tmp_str = re.sub(r'\([ 0-9,–\-]*\)', '', tmp_str)                                                    # (각주) 제거
-    tmp_str = re.sub(r'\[[ 0-9,–\-]*\]', '', tmp_str)                                                    # [각주] 제거
-    tmp_str = re.sub(r'\ ,', ',', tmp_str)                                                               # , 앞 공백 제거
-    tmp_str = re.sub(r'\ \.', '.', tmp_str)                                                              # . 앞 공백 제거
+    tmp_str = re.sub(r'([a-z].*?)(\d[ 0-9,–\-]*\d+)([,.\ ])', '\\1\\3', tmp_str)                            # 각주 제거
+    tmp_str = re.sub(r'\ [ 0-9,–\-]*([ ,.])', '\\1', tmp_str)                                               #  각주 제거
+    tmp_str = re.sub(r'\([ 0-9,–\-]*\)', '', tmp_str)                                                       # (각주) 제거
+    tmp_str = re.sub(r'\[[ 0-9,–\-]*\]', '', tmp_str)                                                       # [각주] 제거
+    tmp_str = re.sub(r'\ ,', ',', tmp_str)                                                                  # , 앞 공백 제거
+    tmp_str = re.sub(r'\ \.', '.', tmp_str)                                                                 # . 앞 공백 제거
     tmp_str = re.sub(r'\.,', ',', tmp_str)
     tmp_str = re.sub(r',,', ',', tmp_str)
     tmp_str = re.sub(r'\.\.', '.', tmp_str )
-    tmp_str = re.sub(r'\ \ ', ' ', tmp_str)                                                              # 2번 연속되는 공백 제거
+    tmp_str = re.sub(r'\ \ ', ' ', tmp_str)                                                                 # 2번 연속되는 공백 제거
     for i in my_dict:
         tmp_str = tmp_str.replace(i + '@', i)
     tmp_str = re.sub(r'(\d)@', '\\1', tmp_str)
@@ -55,11 +54,11 @@ def PyRefmover():                                                               
 
 def toggler(mode, state):
     if state == True:
-        time.sleep(0.1)                                                                                  # 연속 전환 방지
+        time.sleep(0.1)                                                                                     # 연속 전환 방지
         print(mode + ": OFF\r")
         return False
     else:
-        time.sleep(0.1)                                                                                  # 연속 전환 방지
+        time.sleep(0.1)                                                                                     # 연속 전환 방지
         print(mode + ": ON \r")
         return True
 
@@ -69,7 +68,7 @@ my_dict = []
 
 # ====================================================================================================
 
-my_dict_f = open(my_path + '/mydict.txt', "r")
+my_dict_f = open(my_path + '/mydict.txt', "r")                                                              # 각주 제거 예외처리(e.g., CD4, CD8)
 lines = my_dict_f.readlines()
 for line in lines:
     my_dict.append(line.strip())
@@ -107,41 +106,43 @@ while True:
     if select == 'Quit':
         break
 
-    while select == 'Translator':                                                                        # 자동 번역기
-        if keyboard.is_pressed("F2"):                                                                    # F2로 On/Off 토글
+    while select == 'Translator':                                                                           # 자동 번역기
+        if keyboard.is_pressed("F2"):                                                                       # F2로 On/Off 토글
             os.system("cls")
             flag = toggler('번역', flag)
             print("\n뒤로가기(F4)")
-        elif keyboard.is_pressed("F4"):                                                                  # F4로 이전 단계로 이동
+        elif keyboard.is_pressed("F4"):                                                                     # F4로 이전 단계로 이동
             time.sleep(0.1)
             break
-        elif flag and keyboard.is_pressed("Ctrl") and keyboard.is_pressed("c"):                          # Ctrl C 감지
+        elif flag and keyboard.is_pressed("Ctrl") and keyboard.is_pressed("c"):                             # Ctrl C 감지
             os.system('cls')
             print("번역: ON ")
-            text = PyRefmover()                                                                          # 문자열 text에 각주 제거된 문자열 대입
+            text = PyRefmover()                                                                             # 문자열 text에 각주 제거된 문자열 대입
             print("...\r", end = "")
             text = translate_text("ko", text)
             clipboard.copy(text)
+            print("         ")
             print(text)
             print("\n뒤로가기(F4)")
             time.sleep(0.1)
         else:
-            time.sleep(0.05)                                                                             # 속도 제한용 0.05초 대기
+            time.sleep(0.05)                                                                                # 속도 제한용 0.05초 대기
 
-    while select == 'Refmover':                                                                          # 각주 제거기
-        if keyboard.is_pressed("F2"):                                                                    # F2로 On/Off 토글
+    while select == 'Refmover':                                                                             # 각주 제거기
+        if keyboard.is_pressed("F2"):                                                                       # F2로 On/Off 토글
             os.system("cls")
             flag = toggler('각주 제거', flag)
             print("\n뒤로가기(F4)")
-        elif keyboard.is_pressed("F4"):                                                                  # F4로 이전 단계로 이동
+        elif keyboard.is_pressed("F4"):                                                                     # F4로 이전 단계로 이동
             time.sleep(0.1)
             break
-        elif flag and keyboard.is_pressed("Ctrl") and keyboard.is_pressed("c"):                          # Ctrl C 감지
+        elif flag and keyboard.is_pressed("Ctrl") and keyboard.is_pressed("c"):                             # Ctrl C 감지
             os.system("cls")
             print("각주 제거: ON ")
             print("...\r", end = "")
             text = PyRefmover()
-            print("         \n" + text)
+            print("         ")
+            print(text)
             clipboard.copy(text)
             print("\n뒤로가기(F4)")
             time.sleep(0.1)
